7c3ea916752c78d3047d8aa8a20dac62a934d25e
Ola <hollabodey.kilaso@gmail.com>
2023-04-08 11:58:47 +0100

    Made changes to the file structure

--- React-native-jobs/components/home/nearby/Nearbyjobs.jsx
+++ React-native-jobs/components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import {
+  View, Text, TouchableOpacity, FlatList, ActivityIndicator
+} from 'react-native'
 
-import styles from './nearbyjobs.style'
+import { useRouter } from "expo-router";
 
-const Nearbyjobs = () => {
+import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants'; 
+import PopularjobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from '../../../Hook/useFetch';
+
+
+const Popularjobs = () => {
+  const router = useRouter();
+
+  const { data, isLoading, error } = useFetch
+  ('search', {
+    query: 'React developer',
+    num_pages: 1
+  })
+
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator Sizes="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={( item )=> (
+              <PopularjobCard
+                item={item}
+              />
+            )}
+
+            keyExtractor={item =>item ?.job_id}
+            contentContainerStyle={{columGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
 
-export default Nearbyjobs
+export default Popularjobs
